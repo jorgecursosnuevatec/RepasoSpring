73e2133a424bf708c163131d3721f3f5d07b644a
jorgecursosnuevatec <jorgecursosnuevatec@gmail.com>
2023-01-25 07:48:15 +0100

    Wed Jan 25 07:47:38     20232023-01-25 PRIMERA SUBIDA DESDE CASA

---
+++ SpringBasico/3-RepasoThymeleafModelo/src/main/java/com/jgr/repaso/thymeleaf/model/Persona.java
@@ -0,0 +1,11 @@
+package com.jgr.repaso.thymeleaf.model;
+
+import lombok.Data;
+
+@Data
+public class Persona {
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String telefono;
+}
