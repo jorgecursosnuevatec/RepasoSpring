b0fc1bb180522ea03c7edaf30818d72b4ce0a910
jorgecursosnuevatec <jorgecursosnuevatec@gmail.com>
2023-01-26 15:18:06 +0100

    Thu Jan 26 15:16:39     20232023-01-26 OTRA SUBIDA DESDE CASA

--- gitprune.sh
+++ gitprune.sh
@@ -14,7 +14,7 @@ PushLog="/dev/null"
 # Mensaje
 message=$@
 if [ -z "$message" ]; then
-message="$(date)2023-01-26 CRUD UBALDO"
+message="$(date)2023-01-26 OTRA SUBIDA DESDE CASA"
 fi
 
 start=`date +%s`
